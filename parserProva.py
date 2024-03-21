e6311ebcd326fca52c1c3616323233e3d2e30a9b
Andrea Sanguineti <andrea.sanguineti@mail.polimi.it>
2024-03-21 19:31:43 +0100

    shish

--- parserProva.py
+++ parserProva.py
@@ -1,4 +1,35 @@
-from tile import Tile
+moves = {"3": ["LR"],
+         "5": ["DR"],
+         "6": ["LD"],
+         "7": ["LR", "LD", "DR"],
+         "9": ["UR"],
+         "96": ["LD", "UR"],
+         "A": ["LU"],
+         "B": ["LR", "LU", "UR"],
+         "C": ["UD"],
+         "C3": ["LR", "UD"],
+         "D": ["UD", "UR", "DR"],
+         "E": ["LU", "LD", "UD"],
+         "F": ["LR", "LD", "LU", "UD", "DR", "UR"]}
+
+class Tile:
+    def __init__(self, id, cost, available):
+        self.id = id
+        self.cost = int(cost)
+        self.available = int(available)
+
+    def __str__(self):
+        return f"Tyle - Id: {self.id} Cost: {self.cost}, Available: {self.available}"
+
+    def use(self):
+        if self.disponibili > 0:
+            self.disponibili -= 1
+        else:
+            raise Exception("Not available")
+    def can_move(self, f, t):
+        return (f+t in moves[self.id] or t+f in moves[self.id])
+    def is_available(self):
+        return self.available > 0
 
 class GoldenPoint:
     def __init__(self, x, y):
@@ -119,9 +150,12 @@ def end():
             return False
     return True
 
-s = find_nearest_silver_point(golden_points[2])
-current_point = golden_points[0]
-current_point.visited = True
+
+print_grid_ascii(grid)
+print("\n-----------------\n")
+s = find_nearest_silver_point(golden_points[1])
+current_point = golden_points[1]
+#current_point.visited = True
 while not end():
     if s.y > current_point.y:
         for y in range(current_point.y, s.y + 1):
